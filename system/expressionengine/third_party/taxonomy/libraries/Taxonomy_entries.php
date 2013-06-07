ece8512c76cb860dcbf2ae56c9894cb178836b31
Iain Urquhart <iain@jamdigital.co.nz>
2013-06-07 17:35:55 +1200

    taxonomy entries lib

---
+++ system/expressionengine/third_party/taxonomy/libraries/Taxonomy_entries.php
@@ -0,0 +1,33 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+ 
+require_once(PATH_MOD.'channel/mod.channel.php');
+ 
+class Taxonomy_entries extends Channel {
+
+    // -----------------------------------------------------------------
+     
+    /**
+     * Constructor function
+     */
+    function __construct() 
+    {
+        parent::Channel();
+
+    }
+     
+    // -----------------------------------------------------------------
+     
+    /**
+     * entries
+     * 
+     * Adjust channel:entries parameters before it runs
+     * 
+     * @return string 
+     */
+    public function entries()
+	{
+		ee()->TMPL->tagparams['dynamic'] = 'no';
+		return parent::entries();
+    } 
+ 
+}
