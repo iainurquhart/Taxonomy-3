f7a64eb093b2786eeb9ee3efce4e187baca2efc6
Iain Urquhart <iain@jamdigital.co.nz>
2013-10-11 13:14:37 +1300

    nudge version.

--- system/expressionengine/third_party/taxonomy/config.php
+++ system/expressionengine/third_party/taxonomy/config.php
@@ -4,6 +4,6 @@ if (!defined('TAXONOMY_NAME')) define('TAXONOMY_NAME', 'Taxonomy');
 if (!defined('TAXONOMY_SHORT_NAME')) define('TAXONOMY_SHORT_NAME', 'taxonomy');
 if (!defined('TAXONOMY_DESCRIPTION')) define('TAXONOMY_DESCRIPTION', 'Build Navigation');
 if (!defined('TAXONOMY_AUTHOR')) define('TAXONOMY_AUTHOR', 'Iain Urquhart');
-if (!defined('TAXONOMY_VERSION')) define('TAXONOMY_VERSION', '3.0.8');
+if (!defined('TAXONOMY_VERSION')) define('TAXONOMY_VERSION', '3.0.8.1');
 if (!defined('TAXONOMY_URL')) define('TAXONOMY_URL', 'iain.co.nz/taxonomy');
 if (!defined('TAXONOMY_USAGE')) define('TAXONOMY_USAGE', '');
